275e42ec0632935885df5b0473773984ac609643
Gintare <37443286+gintarios@users.noreply.github.com>
2018-11-01 12:10:03 +0000

    TV show images added #2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MovieCover from './img/spiderman.jpg';
+import Movie1 from './img/blackmirror.jpg';
+import Movie2 from './img/breakingbad.jpg';
+import Movie3 from './img/deathnote.jpg';
+import Movie4 from './img/got.jpg';
+import Movie5 from './img/walkingdead.jpg';
+import Movie6 from './img/thewire.jpg';
 
 class App extends Component {
   render() {
@@ -11,11 +17,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to kodflix</h1>
         </header>
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <img src={MovieCover} className="Film" alt="Spider Man" />
+        <img src={MovieCover} className="Film" alt="Spider Man" />
+
+        <div className="Container">        
+            <img src={Movie1} className="MovieImg" alt="Black Mirror" />
+            <img src={Movie2} className="MovieImg" alt="Breaking Bad" />
+            <img src={Movie3} className="MovieImg" alt="Death Note" />
+            <img src={Movie4} className="MovieImg" alt="Got" />
+            <img src={Movie5} className="MovieImg" alt="Fear the Walking Dead" />
+            <img src={Movie6} className="MovieImg" alt="The Wire" />   
+        </div> 
       </div>
+      
+       
+
     );
   }
 }
