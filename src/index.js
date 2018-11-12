902343cf0b497aa824fc0d145bfc64d7d125745b
Gintare <37443286+gintarios@users.noreply.github.com>
2018-11-12 16:02:19 +0000

    Run frontend and backend servers in parallel #23 (#24)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from '../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-dom';
+//import ReactDOM from '../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from '-m 'reactApp';
+import App from './frontend/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
