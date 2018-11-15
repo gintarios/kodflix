62f9d63c8aaa7bffeda672631400a70b084aaae7
Gintare <37443286+gintarios@users.noreply.github.com>
2018-11-15 15:51:49 +0000

    Replace the TV show frontend data source, with the backend one #27

--- src/frontend/Details.js
+++ src/frontend/Details.js
@@ -1,43 +1,73 @@
 import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import movies from "./data-get";
 
 
 class Details extends Component {
 
-    findMovieTitle = () => {
-        const findmovie = movies.find(movie => movie.url === this.props.match.url);
-        return findmovie ? findmovie.movietitle : <Redirect to="/not-found" />
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [],
+            isLoaded: false
+        }
     }
+
+    componentDidMount() {
+        fetch('/rest/shows')
+            .then(response => response.json())
+            .then(json => this.setState({
+                isLoaded: true,
+                movies: json,
+            }))
+    }
+
+
+    findMovieTitle = () => {
+        const { movies } = this.state;
+        const findmovie = movies.find(movie => movie.id === this.props.match.url);
+        return findmovie ? findmovie.title : <Redirect to="/not-found" />
+    }
+
     findSynopsis = () => {
-        const findmovie = movies.find(movie => movie.url === this.props.match.url);
+        const { movies } = this.state;
+        const findmovie = movies.find(movie => movie.id === this.props.match.url);
         return findmovie ? findmovie.synopsis : "No Synopsis found"
     }
     findMovieImg = () => {
-        const findmovie = movies.find(movie => movie.url === this.props.match.url);
-        return findmovie ? findmovie.movieimg : "No Image found"
+        const { movies } = this.state;
+        const findmovie = movies.find(movie => movie.id === this.props.match.url);
+        return findmovie ? findmovie.id : "No Image found"
     }
 
 
-
     render() {
+        const { isLoaded, movies } = this.state;
+        if (!isLoaded) {
+            return <div className="loader">
+                <div className="icon">Loading...</div>
+            </div>
+        } else {
 
-        return (
-            <div>
-                <div className="detailsContainer">
-                    <div className="detailstitle">{this.findMovieTitle()}</div>
-                    <div className="detailsbox detailssynopsis">{this.findSynopsis()}</div>
-                    <div className="detailsbox detailsimage"><img src={this.findMovieImg()} alt="Movie Cover" /></div>
-                    < div className="detailslink" > <Link to="/" exact="true"> <p>Go back to the Home page</p></Link> </div>
-                    <br />
-                </div>
-            </div >
-        );
+            return (
+                <div>
+                    <div className="detailsContainer">
+                        <div className="detailstitle"> {this.findMovieTitle()}</div>
+                        <div className="detailsbox detailssynopsis">{this.findSynopsis()}</div>
+                        <div className="detailsbox detailsimage"><img src={require(`./common/images${this.findMovieImg()}.jpg`)} alt="Movie Cover" /></div>
+                        <div className="detailslink" > <Link to="/" exact="true"> <p>Go back to the Home page</p></Link> </div>
+                        <br />
+                    </div>
+                </div >
+            );
+
+        }
+
     }
 }
 
 
 
 export default Details;
+
 
 
--- src/frontend/Gallery.js
+++ src/frontend/Gallery.js
@@ -1,23 +1,58 @@
 import React, { Component } from 'react';
-import Movie from './Movie';
-import movies from "./data-get";
+// import Movie from './Movie';
+import { Link } from 'react-router-dom';
+// import movies from "./data-get";
 
 
 class Gallery extends Component {
-    render() {
-        return (
-            <div>
-                <div className="Container">
-                    {movies.map(function (movie, index) {
-                        return <Movie url={movies[index].url}
-                            movietitle={movies[index].movietitle}
-                            movieimg={movies[index].movieimg} />
-                    })}
-                </div>
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [],
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        fetch('/rest/shows')
+            .then(response => response.json())
+            .then(json => this.setState({
+                isLoaded: true,
+                movies: json,
+            }))
+            .catch(error => console.log('Error:', error));
+    }
+
+    render() {
+        const { isLoaded, movies } = this.state;
+        if (!isLoaded) {
+            return <div className="loader">
+                <div className="icon">Loading...</div>
             </div>
-        );
+        } else {
+
+            return (
+                <div>
+                    <div className="Container">
+                        {movies.map(function (movie, index) {
+                            return <div className="Column">
+                                <Link to={movie.id} exact="true">
+                                    <div className="overlay">
+                                        <h2>{movie.title}</h2>
+                                    </div>
+                                    <img src={require(`./common/images${movie.id}.jpg`)} alt='Movie Cover' />
+                                </Link>
+                            </div>
+                        })
+                        }
+                    </div>
+
+                </div>
+            );
+        }
     }
 }
+
 
 export default Gallery;
