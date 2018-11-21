5cd72ff24aca7e1e59c5f27da32784aad2232fe9
Gintare <37443286+gintarios@users.noreply.github.com>
2018-11-21 12:35:06 +0000

    App is broken in prod! - point to DEV/PRD database dynamically #34

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,14 +1,20 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+require('dotenv').config();
 
-const url = 'mongodb://localhost:27017/';
 
+const url = process.env.DB_URL_PRD || process.env.DB_URL_DEV;
+
+
+
+const index = url.lastIndexOf("kodflix");
+const database = url.substr(index, 7);
 
 function connect() {
     return new Promise((resolve, reject) => {
         MongoClient.connect(url, function (err, db) {
             assert.equal(null, err);
-            const dbo = db.db('kodflix');
+            const dbo = db.db(database);
             resolve(dbo);
         });
     });
