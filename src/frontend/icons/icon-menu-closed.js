9d26553b46cc2d200771d03e9e8de9a667293d80
Gintare <37443286+gintarios@users.noreply.github.com>
2018-12-11 12:44:39 +0000

    Create the vertical menu panel #38

---
+++ src/frontend/icons/icon-menu-closed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default() =>
+<svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+              >
+                <g fill="#fe4365">
+                  <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+                </g>
+              </svg>
---
+++ src/frontend/icons/icon-menu-tvshows.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => 
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="28"
+    height="28"
+    viewBox="0 0 24 24"
+  >
+    <g fill="#fe4365">
+      <path d="M13.112 6l2.888-3.375-.781-.625-3.219 3.75-3.219-3.75-.781.625 2.888 3.375h-10.888v16h24v-16h-10.888zm7.888 14h-18v-12h18v12zm-12-2v-8l7 4-7 4z" />
+    </g>
+  </svg>
+
